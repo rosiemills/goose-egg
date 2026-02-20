eed6e9a83df7ca259ae71f32553ee8e16e6e6329
elizy200 <eca_family@icloud.com>
2026-02-20 14:16:37 -0600

    start to tutorial level + camera follows goose
    i created the ground on the tutorial level and then got bored and decided to figure out how to get the camera to follow the goose and then i realized maybe those should've been separate pulls but too little too late yk 🤷‍♀️

---
+++ goose egg/Assets/SmoothCameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float damping;
+
+    public Transform target;
+
+    private Vector3 vel = Vector3.zero;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+
+        targetPosition.z = transform.position.z;
+        
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+    }
+}
