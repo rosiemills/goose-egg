66115a81504b3c54527dd12c6616ec1824b7256e
rosiemills <danica.sumaling@gmail.com>
2026-04-21 14:12:36 -0500

    update enemy script

--- goose egg/Assets/Goose/PlayerCombat.cs
+++ goose egg/Assets/Goose/PlayerCombat.cs
@@ -10,10 +10,11 @@ public class PlayerCombat : MonoBehaviour
     public Animator animator;
     public Transform attackPoint;
     public LayerMask enemyLayers;
+    
 
     public float attackRange = 0.5f;
     public int attackDamage = 40;
-
+    
     public float attackRate = 2f; 
     float nextAttackTime = 0f;
 
@@ -41,9 +42,13 @@ public class PlayerCombat : MonoBehaviour
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
         
         // Damage them
-        foreach(Collider2D enemy in hitEnemies)
+        foreach(Collider2D e in hitEnemies)
         {
-            UnityEngine.Debug.Log("We hit " + enemy.name);
+            Enemy enemy = e.GetComponent<Enemy>();
+            if(enemy != null)
+            {
+                enemy.TakeDamage(attackDamage);
+            }
         }
     }
 
--- goose egg/Assets/Scripts/Enemy.cs
+++ goose egg/Assets/Scripts/Enemy.cs
@@ -1,19 +1,64 @@
 using System.Diagnostics;
+using System.Numerics;
+//using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
+    public float speed;
+    
+    //two opposite points for the patrol region
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Transform currentPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+        animator.SetBool("IsRunning", true);
+
+    }
+
+    void Update()
+    {
+        UnityEngine.Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.linearVelocity = new UnityEngine.Vector2(speed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new UnityEngine.Vector2(-speed, 0);
+        }
+
+        if(UnityEngine.Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if(UnityEngine.Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
 
         if(currentHealth <= 0)
         {
@@ -24,5 +69,10 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         UnityEngine.Debug.Log("Enemy died!");
+
+        animator.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
     }
 }
