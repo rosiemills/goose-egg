612f5ec68a22a743ff3eb964d0eef9439f4a422d
rosiemills <danica.sumaling@gmail.com>
2026-05-12 19:55:31 -0500

    enemy pathing
    added to enemy patrol pattern, changed some preferences/settings (added vs code package - not sure what it's doing exactly) - still in progress

--- goose egg/Assets/Scripts/Enemy.cs
+++ goose egg/Assets/Scripts/Enemy.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Drawing;
 using System.Numerics;
+
 //using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
@@ -14,9 +16,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private float speed = 3f;
     [SerializeField] private int startDirection = 1;
+    [SerializeField] private bool stayOnLedges = true;
     private int currentDirection;
     private float halfWidth;
+    private float halfHeight;
     private UnityEngine.Vector2 movement;
+    private bool isGrounded;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,7 +30,9 @@ public class Enemy : MonoBehaviour
         currentHealth = maxHealth;
         animator.SetBool("IsRunning", true);
         halfWidth = spriteRenderer.bounds.extents.x;
+        halfHeight = spriteRenderer.bounds.extents.y;
         currentDirection = startDirection;
+        spriteRenderer.flipX = startDirection == 1 ? false : true;
     }
 
     private void FixedUpdate()
@@ -33,6 +40,70 @@ public class Enemy : MonoBehaviour
         movement.x = speed * currentDirection;
         movement.y = rb.linearVelocity.y;
         rb.linearVelocity = movement;
+        SetDirection();
+    }
+
+
+    private void onCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Default"))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void onCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+
+    //Changes direction after hitting a wall or ledge
+    private void SetDirection()
+    {
+        if(!isGrounded) return;   //stops sprite from spinning when not grounded
+
+        UnityEngine.Vector2 rightPos = transform.position;
+        UnityEngine.Vector2 leftPos = transform.position;
+        rightPos.x += halfWidth;
+        leftPos.x -= halfWidth;
+
+        if(rb.linearVelocity.x > 0)
+        {
+            if (Physics2D.Raycast(transform.position, UnityEngine.Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Default")))
+            {
+                //  Draw a ray starting at the center of our enemy and point it to the right
+                //  check to see if the raycast is intersecting with a wall
+                //  Also check to make sure our enemy is actually WALKING right
+                //  If we don't do this check the enemy will get stuck moving constantly back and forth
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }
+            else if (stayOnLedges && !Physics2D.Raycast(rightPos, UnityEngine.Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Default")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }
+        }
+        else if(rb.linearVelocity.x < 0)
+        {
+            if (Physics2D.Raycast(transform.position, UnityEngine.Vector2.left, halfWidth + 0.1f, LayerMask.GetMask("Default")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+            }
+            else if (stayOnLedges && !Physics2D.Raycast(leftPos, UnityEngine.Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Default")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+            }
+        }
+
+        // shows the raycast physically (easier to see what it hits)
+        //UnityEngine.Debug.DrawRay(transform.position, UnityEngine.Vector2.right * (halfWidth + 0.1f), UnityEngine.Color.red);
     }
 
     public void TakeDamage(int damage)
